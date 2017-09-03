9a0b6d8200f90c67078a9b5838b3fe99b1db7a86
satotake <doublequotaion@gmail.com>
2017-09-04 00:37:57 +0900

    init eslintrc,

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>this is test</Text>
-      </View>
-    );
-  }
-}
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +13,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>this is test</Text>
+  </View>
+);
+
+module.exports = App;
