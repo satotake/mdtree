6034d9a795e07e1d30c48d6aeb709af342877965
SatowTakeshi <doublequotation@gmail.com>
2017-09-08 23:59:58 +0900

    write test: red

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,132 +1,488 @@
-import fs from 'fs';
 import {
   markdown,
 } from 'markdown';
 
 import Helpers from './index';
 
-const testMDDir = '/test_mds/';
-
-const readAndParseMD = (filename) => {
-  const raw = fs.readFileSync(`${__dirname}${testMDDir}${filename}`, 'utf8');
-  return markdown.parse(raw);
-};
-
 describe('nestByHeadingLevel', () => {
   const callFUT = Helpers.nestByHeadingLevel;
+
   it('empty', () => {
-    const tree = readAndParseMD('empty.md');
-    // [ 'markdown', [ 'para', 'no heading' ] ]
+    const raw = '';
+    const tree = markdown.parse(raw);
+    // ['markdown']
     const actual = callFUT(tree);
+    const expected = {
+      meta: {
+        doctype: 'markdown',
+      },
+    };
     expect(actual)
-      .toBe(true);
+      .to.deep.have.same.members(expected);
   });
   it('no heading', () => {
-    const tree = readAndParseMD('no_h.md');
-    // [ 'markdown', [ 'para', 'no heading' ] ]
+    const raw = `
+no
+
+heading
+`;
+    const tree = markdown.parse(raw);
+    // [ 'markdown', [ 'para', 'no' ], [ 'para', 'heading' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        body: [
+          ['para', 'no'],
+          ['para', 'heading'],
+        ],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1 only', () => {
-    const tree = readAndParseMD('h1.md');
+    const raw = '# H1';
+    const tree = markdown.parse(raw);
     // [ 'markdown', [ 'header', { level: 1 }, 'H1' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'H1'],
+          ],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
+  it('para, H1', () => {
+    const raw = `
+paragraph
+# H1
+`;
+    const tree = markdown.parse(raw);
+    /* [ 'markdown',
+     *   [ 'para', 'paragraph' ],
+     *   [ 'header', { level: 1 }, 'H1' ] ] */
+    const actual = callFUT(tree);
+
+    const expected = {
+      root: {
+        level: 0,
+        body: [
+          ['para', 'paragraph'],
+        ],
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'H1'],
+          ],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
+  });
+
   it('H1 only without text', () => {
-    const tree = readAndParseMD('h1_without_text.md');
+    const raw = '# ';
+    const tree = markdown.parse(raw);
     // [ 'markdown', [ 'header', { level: 1 } ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }],
+          ],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H3 only', () => {
-    const tree = readAndParseMD('h3.md');
+    const raw = '### h3';
+    const tree = markdown.parse(raw);
     // [ 'markdown', [ 'header', { level: 3 }, 'h3' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          ch: [{
+            level: 2,
+            ch: [{
+              level: 3,
+              body: [
+                ['header', {
+                  level: 3,
+                }, 'h3'],
+              ],
+            }],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1, H2, H3', () => {
-    const tree = readAndParseMD('h1_to_h3.md');
+    const raw = `
+# h1
+## h2
+### h3
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'h1' ],
     //   [ 'header', { level: 2 }, 'h2' ],
     //   [ 'header', { level: 3 }, 'h3' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2'],
+            ],
+            ch: [{
+              level: 3,
+              body: [
+                ['header', {
+                  level: 3,
+                }, 'h3'],
+              ],
+            }],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1, H2, H2 , H3', () => {
-    const tree = readAndParseMD('h1_to_h2_then_h2_to_h3.md');
+    const raw = `
+# h1
+## h2-1
+## h2-2
+### h3
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'h1' ],
-    //   [ 'header', { level: 2 }, 'h2' ],
-    //   [ 'header', { level: 2 }, 'h2' ],
+    //   [ 'header', { level: 2 }, 'h2-1' ],
+    //   [ 'header', { level: 2 }, 'h2-2' ],
     //   [ 'header', { level: 3 }, 'h3' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2-1'],
+            ],
+          }, {
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2-2'],
+            ],
+            ch: [{
+              level: 3,
+              body: [
+                ['header', {
+                  level: 3,
+                }, 'h3'],
+              ],
+            }],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1, H2, H3, H1, H2', () => {
-    const tree = readAndParseMD('h1_to_h3_then_h1_to_h2.md');
+    const raw = `
+# h1
+## h2
+### h3
+
+# h1-2
+## h2-2
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'h1' ],
     //   [ 'header', { level: 2 }, 'h2' ],
     //   [ 'header', { level: 3 }, 'h3' ],
-    //   [ 'header', { level: 1 }, 'h1' ],
-    //   [ 'header', { level: 2 }, 'h2' ] ]
+    //   [ 'header', { level: 1 }, 'h1-2' ],
+    //   [ 'header', { level: 2 }, 'h2-2' ] ]
 
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2'],
+            ],
+            ch: [{
+              level: 3,
+              body: [
+                ['header', {
+                  level: 3,
+                }, 'h3'],
+              ],
+            }],
+          }],
+        }, {
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1-2'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2-2'],
+            ],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1, H2, H3, H2', () => {
-    const tree = readAndParseMD('h1_to_h3_then_h2.md');
+    const raw = `
+# h1
+## h2
+### h3
+## h2-2
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'h1' ],
     //   [ 'header', { level: 2 }, 'h2' ],
     //   [ 'header', { level: 3 }, 'h3' ],
-    //   [ 'header', { level: 2 }, 'h2' ] ]
+    //   [ 'header', { level: 2 }, 'h2-2' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2'],
+            ],
+            ch: [{
+              level: 3,
+              body: [
+                ['header', {
+                  level: 3,
+                }, 'h3'],
+              ],
+            }],
+          }],
+        }, {
+          level: 1,
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2-2'],
+            ],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1, H2, H4', () => {
-    const tree = readAndParseMD('h1_to_h2_then_h4.md');
+    const raw = `
+# h1
+## h2
+#### h4
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'h1' ],
     //   [ 'header', { level: 2 }, 'h2' ],
     //   [ 'header', { level: 4 }, 'h4' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2'],
+            ],
+            ch: [{
+              level: 3,
+              ch: [{
+                level: 4,
+                body: [
+                  ['header', {
+                    level: 4,
+                  }, 'h4'],
+                ],
+              }],
+            }],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
   it('H1 para, H2 para, H3 para', () => {
-    const tree = readAndParseMD('h1_to_h3_with_paras.md');
+    const raw = `
+# h1
+
+paragraph1
+
+## h2
+
+paragraph2
+
+### h3
+paragraph3
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'h1' ],
-    //   [ 'para', 'paragraph' ],
+    //   [ 'para', 'paragraph1' ],
     //   [ 'header', { level: 2 }, 'h2' ],
-    //   [ 'para', 'paragraph' ],
+    //   [ 'para', 'paragraph2' ],
     //   [ 'header', { level: 3 }, 'h3' ],
-    //   [ 'para', 'paragraph' ] ]
+    //   [ 'para', 'paragraph3' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = {
+      root: {
+        level: 0,
+        ch: [{
+          level: 1,
+          body: [
+            ['header', {
+              level: 1,
+            }, 'h1'],
+            ['para', 'paragraph1'],
+          ],
+          ch: [{
+            level: 2,
+            body: [
+              ['header', {
+                level: 2,
+              }, 'h2'],
+              ['para', 'paragraph2'],
+            ],
+            ch: [{
+              level: 3,
+              body: [
+                ['header', {
+                  level: 3,
+                }, 'h3'],
+                ['para', 'paragraph3'],
+              ],
+            }],
+          }],
+        }],
+      },
+    };
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
-  it('H1 para, H2 para, H3 para', () => {
-    const tree = readAndParseMD('h1_to_h3_with_header.md');
+
+  /* TODO(markdown-js cannot distinguish header from #header) */
+  xit('header, subheader, H1 para, H2 para, H3 para', () => {
+    const raw = `
+header
+===
+subheader
+---
+
+# h1
+`;
+    const tree = markdown.parse(raw);
     // [ 'markdown',
     //   [ 'header', { level: 1 }, 'header' ],
     //   [ 'header', { level: 2 }, 'subheader' ],
     //   [ 'header', { level: 1 }, 'h1' ],
-    //   [ 'header', { level: 2 }, 'h2' ],
-    //   [ 'header', { level: 3 }, 'h3' ] ]
     const actual = callFUT(tree);
-    expect(actual)
-      .toBe(true);
+    const expected = false;
+    expect(actual.root)
+      .to.have.members(expected.root);
   });
 });
