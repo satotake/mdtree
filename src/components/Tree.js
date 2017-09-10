5e5f33b3860f90b2c419d2c2915b5e32d9fe2ced
SatowTakeshi <doublequotation@gmail.com>
2017-09-10 18:48:22 +0900

    add fn `onButtonPress` to Tree component

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
+  Button,
   Text,
+  View,
 } from 'react-native';
 
-const Tree = () => (<Text> TreeScreen </Text>);
+const Tree = ({ onButtonPress }) => (
+  <View>
+    <Text> TreeScreen </Text>
+    <Button
+      title="dispatch HOME"
+      onPress={() => onButtonPress()}
+    />
+  </View>
+);
+
+Tree.propTypes = {
+  onButtonPress: PropTypes.func.isRequired,
+};
 
 export default Tree;
--- src/containers/TreeScreen.js
+++ src/containers/TreeScreen.js
@@ -1,6 +1,16 @@
 import { connect } from 'react-redux';
 
+import Actions from '../actions';
+
 import Tree from '../components/Tree';
 
-const TreeScreen = connect()(Tree);
+const mapDispatchToProps = dispatch => ({
+  onButtonPress: () => dispatch(Actions.switchNavi.home()),
+});
+
+const TreeScreen = connect(null, mapDispatchToProps)(Tree);
+TreeScreen.navigationOptions = {
+  title: 'Tree',
+};
+
 export default TreeScreen;
